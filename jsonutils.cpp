faddc04619d850be53f627100283aa191a1a2bb4
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-02-28 21:39:23 -0500

    Created toValue and valueToTree

--- jsonutils.cpp
+++ jsonutils.cpp
@@ -11,7 +11,8 @@ namespace jsonutils
 	std::string Stringifiable::stringify() const
 	{
 		rapidjson::Document doc;
-		writeToDocument(doc);
+		doc.SetObject();
+		writeToObject(doc, &doc.GetAllocator());
 
 		// Set up a string buffer to write the JSON to.
 		rapidjson::StringBuffer buffer;
@@ -19,6 +20,93 @@ namespace jsonutils
 		doc.Accept(writer);
 
 		return buffer.GetString();
+	}
+
+	rapidjson::Value Stringifiable::toValue(rapidjson::MemoryPoolAllocator<rapidjson::CrtAllocator>* allocator) const
+	{
+		rapidjson::Value node;
+		node.SetObject();
+		writeToObject(node, allocator);
+		return node;
+	}
+
+	std::string valueToTree(const rapidjson::Value& value, int indent)
+	{
+		std::string tree;
+		std::string indentStr(indent, ' ');
+
+		// If the value is an object, print its members.
+		if (value.IsObject())
+		{
+			tree += "{\n";
+			for (auto it = value.MemberBegin(); it != value.MemberEnd(); ++it)
+			{
+				tree += indentStr + "  " + it->name.GetString() + ": " + valueToTree(it->value, indent + 1) + "\n";
+			}
+			tree += indentStr + "}";
+		}
+
+		// If the value is an array, print its elements.
+		else if (value.IsArray())
+		{
+			tree += "[\n";
+			for (rapidjson::SizeType i = 0; i < value.Size(); i++)
+			{
+				tree += indentStr + "  " + valueToTree(value[i], indent + 1) + "\n";
+			}
+			tree += indentStr + "]";
+		}
+
+		// If the value is a string, print it.
+		else if (value.IsString())
+		{
+			tree += "\"" + std::string(value.GetString(), value.GetStringLength()) + "\"";
+		}
+
+		// If the value is a number, print it.
+		else if (value.IsNumber())
+		{
+			if (value.IsInt())
+			{
+				tree += std::to_string(value.GetInt());
+			}
+			else if (value.IsUint())
+			{
+				tree += std::to_string(value.GetUint());
+			}
+			else if (value.IsInt64())
+			{
+				tree += std::to_string(value.GetInt64());
+			}
+			else if (value.IsUint64())
+			{
+				tree += std::to_string(value.GetUint64());
+			}
+			else if (value.IsDouble())
+			{
+				tree += std::to_string(value.GetDouble());
+			}
+		}
+
+		// If the value is a boolean, print it.
+		else if (value.IsBool())
+		{
+			tree += value.GetBool() ? "true" : "false";
+		}
+
+		// If the value is null, print it.
+		else if (value.IsNull())
+		{
+			tree += "null";
+		}
+
+		// If the value is something else, print it.
+		else
+		{
+			tree += "???";
+		}
+
+		return tree;
 	}
 
 }
--- jsonutils.h
+++ jsonutils.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <string>
 #include <rapidjson/document.h>
+#include <iostream>
 
 namespace jsonutils
 {
 
-	/* Expects T to have a constructor that takes in an ObjDocument */
-	template <typename T> inline T parse(const std::string& json)
+	std::string	valueToTree(const rapidjson::Value& value, int indent = 0);
+
+	/* Expects T to have a constructor that takes in a Value */
+	template <typename T> inline T parse(const std::string json)
 	{
 		// Template functions must be defined in the header file.
 		// Use inline to avoid multiple definition errors when including this header in multiple files.
@@ -21,8 +24,11 @@ namespace jsonutils
 	{
 	public:
 		std::string stringify() const; // Const at the end of a function declaration means that the function will not modify the object it is called on.
+		rapidjson::Value toValue(rapidjson::MemoryPoolAllocator<rapidjson::CrtAllocator>* allocator) const;
+
 	protected:
-		virtual void writeToDocument(rapidjson::Document& doc) const = 0; // = 0 means this is a pure virtual function (abstract function), and the class is abstract.
+		virtual void writeToObject(rapidjson::Value& node, rapidjson::MemoryPoolAllocator<rapidjson::CrtAllocator>* allocator) const = 0;
+		// = 0 means this is a pure virtual function (abstract function), and the class is abstract.
 	};
 
 }
