ce2e9e8756eee535e85163573f47c76eabb08024
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-02-29 21:24:34 -0500

    Things don't work!

--- jsonutils.h
+++ jsonutils.h
@@ -2,11 +2,24 @@
 #include <string>
 #include <rapidjson/document.h>
 #include <iostream>
+#include <vector>
+#include <array>
 
 namespace jsonutils
 {
 
 	std::string	valueToTree(const rapidjson::Value& value, int indent = 0);
+
+	template <typename T, int size> inline std::array<T, size> toArray(
+		const rapidjson::GenericArray<false, rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::MemoryPoolAllocator<rapidjson::CrtAllocator>>>* value)
+	{
+		std::array<T, size> result;
+		for (rapidjson::SizeType i = 0; i < value.Size(); i++)
+		{
+			result[i] = value[i];
+		}
+		return result;
+	}
 
 	/* Expects T to have a constructor that takes in a Value */
 	template <typename T> inline T parse(const std::string json)
